5319ef932f7d984e750201d70f9c3de5ab2edad7
Vladis1av <vladfrolovcsgo@gmail.com>
2021-09-23 06:40:30 +0300

    Изменил переменные API

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   env: {
     API_URL: process.env.API_URL,
     IMAGE_URL: process.env.IMAGE_URL,
+    ANILIST_API: process.env.ANILIST_API,
     CLIENT_API: process.env.CLIENT_API,
   },
 };
