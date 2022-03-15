114b02053604320b2aa870139f3e480bbe913fd2
vladis1av <vladfrolov.dev@gmail.com>
2022-03-15 21:02:34 +0700

    rework _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,62 +1,85 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
-import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 
-import Header from '../components/Header/Header';
+import Header from '@components/Header/Header';
+import { APP_NAME, SEO_DESCRIPTION, SEO_KEYWORDS_APP } from '@constants/seo';
+import { Colors, Themes } from '@enums/enums';
+import { ThemeType } from '@interfaces/interfaces';
 import { dark, light } from '../theme';
 
-import '../styles/globals.scss';
+import '@styles/globals.scss';
 import 'macro-css';
 
+const themeFromLocalStorage: string = 'anime-APP-theme';
+const nextNProgressStartPosition: number = 0.3;
+const nextNProgressHeight: number = 3;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState<ThemeType>(Themes.dark);
 
   const onChangeTheme = () => {
-    if (theme === 'dark') {
-      setTheme('light');
-      window.localStorage.setItem('anime-APP-theme', 'light');
+    if (theme === Themes.dark) {
+      setTheme(Themes.light);
+      window.localStorage.setItem(themeFromLocalStorage, Themes.light);
     } else {
-      setTheme('dark');
-      window.localStorage.setItem('anime-APP-theme', 'dark');
+      setTheme(Themes.dark);
+      window.localStorage.setItem(themeFromLocalStorage, Themes.dark);
     }
   };
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem('anime-APP-theme');
-    localTheme ? setTheme(localTheme) : setTheme('dark');
-
+    const localTheme = window.localStorage.getItem(themeFromLocalStorage);
     const jssStyles = document.querySelector('#jss-server-side');
+
+    if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      setTheme(Themes.dark);
+    }
+
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles?.parentNode?.removeChild(jssStyles);
     }
   }, []);
 
   return (
     <>
       <Head>
-        <title>Anime APP</title>
-        <meta property="og:site_name" content="Anime APP" />
+        <title>{APP_NAME}</title>
         <meta
-          name="description"
-          content="На сайте вы можете посмотреть онлайн или скачать через торрент аниме."
+          content={APP_NAME}
+          property="og:site_name"
         />
         <meta
-          content="Anime APP,Anime app,anime app,анимe эпп,аниме апп,аниме приложение, смотреть аниme, скачать аниме, аниме бесплатно,  Скачать аниме, аниме торрент, аниме на русском"
+          content={SEO_DESCRIPTION}
+          name="description"
+        />
+        <meta
+          content={SEO_KEYWORDS_APP}
           name="Keywords"
         />
-        <meta content="Anime APP" property="og:title" />
+        <meta
+          content={APP_NAME}
+          property="og:title"
+        />
       </Head>
-      <MuiThemeProvider theme={theme !== 'dark' ? { ...light } : { ...dark }}>
+
+      <MuiThemeProvider theme={theme !== Themes.dark ? { ...light } : { ...dark }}>
         <Header onChangeTheme={onChangeTheme} />
+
         <CssBaseline />
+
         <NextNProgress
-          color="#FFF"
-          startPosition={0.3}
-          height={3}
+          color={Colors.white}
+          startPosition={nextNProgressStartPosition}
+          height={nextNProgressHeight}
           options={{ showSpinner: false }}
         />
+
         <Component {...pageProps} />
       </MuiThemeProvider>
     </>
