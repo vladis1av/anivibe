d41906e34f251f56cace6fccd30fb6b920f44603
vladis1av <vladfrolov.dev@gmail.com>
2022-03-15 20:58:18 +0700

    Fix: imports

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,23 +1,24 @@
-import Paper from '@material-ui/core/Paper';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import Link from '@material-ui/core/Link';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { FC } from 'react';
+import {
+  Paper,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+import { blue, green, pink } from '@material-ui/core/colors';
+import { format, fromUnixTime } from 'date-fns';
+import { ITorrents } from '@interfaces/interfaces';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import CheckIcon from '@material-ui/icons/Check';
-import { Table } from '@material-ui/core/';
-import { blue, green, pink } from '@material-ui/core/colors';
-import fromUnixTime from 'date-fns/fromUnixTime';
-import format from 'date-fns/format';
+import getFileSize from '@utils/getFileSize';
 
-import { ITorrents } from '../../interfaces/torrents';
-import getFileSize from '../../utils/getFileSize';
-
-const TableBlock: React.FC<ITorrents> = ({ list }) => {
+const TableBlock: FC<ITorrents> = ({ list }) => {
   return (
     <Paper>
       <TableContainer>
@@ -25,46 +26,62 @@ const TableBlock: React.FC<ITorrents> = ({ list }) => {
           <TableHead>
             <TableRow>
               <TableCell align="left">Серии</TableCell>
+
               <TableCell align="center">Качество</TableCell>
+
               <TableCell align="center">Размер</TableCell>
+
               <TableCell align="center">Обновление</TableCell>
+
               <TableCell align="center" style={{ color: blue[700] }}>
                 Скачать
               </TableCell>
+
               <TableCell align="center" style={{ color: green[500] }}>
                 <ArrowUpwardIcon />
               </TableCell>
+
               <TableCell align="center">
                 <ArrowDownwardIcon style={{ color: pink[600] }} />
               </TableCell>
+
               <TableCell align="center">
                 <CheckIcon />
               </TableCell>
+
             </TableRow>
           </TableHead>
+
           <TableBody>
             {list.map((item, i) => (
               <TableRow key={i}>
                 <TableCell>{item.series.string}</TableCell>
+
                 <TableCell align="center">{item.quality.string}</TableCell>
+
                 <TableCell align="center">
                   {getFileSize(item.total_size)}
                 </TableCell>
+
                 <TableCell align="center">{`${format(
                   fromUnixTime(item.uploaded_timestamp),
                   'dd.mm.yyyy\u00A0HH:mm',
                 )}`}</TableCell>
+
                 <TableCell align="center">
                   <Link href={`https://tv2.darklibria.it${item.url}`}>
                     <GetAppIcon style={{ color: blue[700] }} />
                   </Link>
                 </TableCell>
+
                 <TableCell align="center" style={{ color: green[500] }}>
                   {item.seeders}
                 </TableCell>
+
                 <TableCell align="center" style={{ color: pink[600] }}>
                   {item.leechers}
                 </TableCell>
+
                 <TableCell align="center">{item.downloads}</TableCell>
               </TableRow>
             ))}
