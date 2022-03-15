29731889c94eaa2fca4554fe7e5036804cc12e1f
vladis1av <vladfrolov.dev@gmail.com>
2022-03-15 20:58:34 +0700

    Fix: props

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import styles from './VideoPlayer.module.scss';
 
 interface VideoPlayerProps {
-  alternative_player: string;
+  alternativePlayer: string;
 }
 
-const VideoPlayer: React.FC<VideoPlayerProps> = ({ alternative_player }) => {
-  if (!alternative_player) {
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ alternativePlayer }) => {
+  if (!alternativePlayer) {
     return null;
   }
 
   return (
     <div id="playerjs" className={styles.videoPlayer}>
       <iframe
-        src={`${alternative_player ? alternative_player : ''}`}
+        src={`${alternativePlayer ? alternativePlayer : ''}`}
         width="100%"
         height="100%"
         frameBorder="0"
