b8e4e454ea5d66d17a41ae7bf56ab644d86b35ea
Vlad Frolov~ <vladfrolov.dev@gmail.com>
2022-03-24 16:26:06 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   env: {
     API_URL: process.env.API_URL,
-    IMAGE_URL: process.env.IMAGE_URL,
-    ANILIST_API: process.env.ANILIST_API,
     CLIENT_API: process.env.CLIENT_API,
+    ANILIST_API_KEY: process.env.ANILIST_API_KEY,
+    IMAGE_URL: process.env.IMAGE_URL,
+    TORRENT_URL: process.env.TORRENT_URL,
   },
 };
