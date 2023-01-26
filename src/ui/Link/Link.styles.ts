84a77085143332883b20fcbb234fb0496e38da67
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-01-26 15:27:04 +0700

    add link component

---
+++ src/ui/Link/Link.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles';
+
+const useLinkStyles = makeStyles(() => ({
+  link: {
+    textDecoration: 'none',
+  },
+}));
+
+export default useLinkStyles;
---
+++ src/ui/Link/Link.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from 'react';
+
+import NextLink from 'next/link';
+
+import clsx from 'clsx';
+
+import useCardListStyles from './Link.styles';
+
+type LinkProps = {
+  path: string;
+  query?: { [id: string]: string } | string;
+  children: ReactNode;
+  className?: string;
+  onClick?: ((e: any) => void) | undefined;
+  scroll?: boolean;
+};
+
+const Link: FC<LinkProps> = ({
+  path,
+  query,
+  children,
+  className,
+  onClick,
+  scroll = true,
+}) => {
+  const classes = useCardListStyles();
+  const currentStyles = clsx(classes.link, className);
+
+  return <NextLink href={{ pathname: path, query }} scroll={scroll} onClick={onClick}>
+    <a className={currentStyles}>
+      {children}
+    </a>
+  </NextLink>;
+};
+
+export default Link;
