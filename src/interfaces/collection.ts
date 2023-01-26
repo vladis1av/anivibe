3f599c2c4f3a2350dc5dde1d3e9acddd5fb6d8c0
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-01-26 15:17:21 +0700

    add collection types

---
+++ src/interfaces/collection.ts
@@ -0,0 +1,16 @@
+import { ECollection } from '@enums/enums';
+
+import { MangaBase } from './manga';
+import { FetchedLastAnimeUpdatedResult } from './services';
+
+export type ECollectionType = keyof typeof ECollection;
+
+// m = Manga, a = Anime
+export type CollectionDataType<M, A> = [ECollection.manga, M] |
+[ECollection.anime, A];
+
+export type CollectionType = {
+  title: string;
+  type: ECollectionType;
+  collection: (MangaBase[] | FetchedLastAnimeUpdatedResult[]) | [];
+};
