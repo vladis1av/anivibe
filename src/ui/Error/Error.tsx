0696dc2adf535a5c1d67085643355df15500b986
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-01-26 15:25:37 +0700

    add error component

---
+++ src/ui/Error/Error.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+
+import Typography from '@mui/material/Typography';
+
+import { ELinkPath } from '@enums/enums';
+
+import Link from '@ui/Link';
+
+import useErrorStyles from './Error.styles';
+
+type ErrorProps = {
+  statusCode?: number;
+  errorText: string;
+  goHome?: boolean;
+};
+
+const Error: FC<ErrorProps> = ({ statusCode, errorText, goHome }) => {
+  const classes = useErrorStyles();
+
+  return (
+    <div className={classes.errorContainer}>
+      {errorText && (
+        <Typography className={classes.errorTitle} align="center" variant="h5">
+          {errorText}
+        </Typography>
+      )}
+
+      <br />
+
+      <Typography align="center" variant="h6">
+        {statusCode
+          && `Ошибка ${statusCode}`}
+      </Typography>
+
+      <br />
+
+      {
+        goHome ? <Link path={ELinkPath.home} className={classes.link}>Вернуться на главную?</Link> : <></>
+      }
+    </div>
+  );
+};
+
+export default Error;
