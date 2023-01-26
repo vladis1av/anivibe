2bf8b632ec981cc86f6dd12c651ac94baf7979fa
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-01-26 15:02:03 +0700

    add constant seo

---
+++ src/constants/seo.ts
@@ -0,0 +1,14 @@
+export const APP_NAME: string = 'Anime APP';
+
+export const SEO_TITLE: string = `Читать мангу или смотреть аниме и скачать его через торрент на ${APP_NAME}`;
+
+export const SEO_DESCRIPTION: string = `На сайте вы можете смотреть онлайн аниме скачать его через торрент,
+ а также читать мангу`;
+
+export const SEO_KEYWORDS_APP: string = `Anime APP,Anime app,anime app,анимe эпп,аниме апп,аниме приложение,
+ смотреть аниme,скачать аниме,аниме бесплатно,Скачать аниме,аниме торрент,аниме на русском,
+ читать мангу,читать мангу бесплатно,манга бесплатно читать онлайн`;
+
+export const SEO_ANIME_DETAIL_PAGE_TITLE: string = 'Смотреть аниме или скачать его на Anime APP';
+
+export const SEO_READ_MANGA_DESCRIPTION: string = 'Легко и удобно читать онлайн мангу на Anime APP';
