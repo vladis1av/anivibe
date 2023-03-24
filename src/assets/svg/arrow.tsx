bb70e26f6dc403dd8ea947fa6c1afffc799b02ae
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 04:36:16 +0700

    add new arrow svg

--- src/assets/svg/arrow.tsx
+++ src/assets/svg/arrow.tsx
@@ -6,8 +6,8 @@ import { EColor } from '@enums/enums';
 
 const ArrowSVG: FC<SvgIconProps> = ({
   fill = EColor.white,
-  width = 25,
-  height = 25,
+  width = 20,
+  height = 20,
   className,
 }) => (
   <svg
@@ -15,13 +15,11 @@ const ArrowSVG: FC<SvgIconProps> = ({
     width={width}
     height={height}
     className={className}
-    viewBox="0 0 17 36"
+    viewBox="0 0 12 16"
   >
     <path
       fill={fill}
-      fillRule="evenodd"
-      d="M5.5 18L16.6 3.2 13.4.8.5 18l12.9 17.2 3.2-2.4L5.5 18z"
-      clipRule="evenodd"
+      d="M4.773 8l3.363 3.364a.9.9 0 11-1.272 1.272l-4-4a.9.9 0 010-1.272l4-4a.9.9 0 111.272 1.272z"
     ></path>
   </svg>
 );
