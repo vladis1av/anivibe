3cb8b39f9ac35e7effb46993c48717df2a38b51a
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 04:53:22 +0700

    add matchMedia constants

---
+++ src/constants/matchMedia.ts
@@ -0,0 +1,5 @@
+export const FILTER_MENU_MATCH_MEDIA: string[] = [
+  '(min-width: 0px) and (max-width: 1025px)',
+];
+
+export const PAGINATION_MATCH_MEDIA: string[] = ['(min-width: 0px) and (max-width: 479px)'];
