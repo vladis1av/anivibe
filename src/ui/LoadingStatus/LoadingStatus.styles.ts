d23bef803b0b2ebf08f69bbaa784a07b0984e20c
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 05:32:01 +0700

    add loadingStatus component

---
+++ src/ui/LoadingStatus/LoadingStatus.styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+const useLoadingStatusStyles = makeStyles(() => ({
+  loadingInfo: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
+
+export default useLoadingStatusStyles;
