036809fba6c37cde491b05bb914ab499d41085be
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 05:15:50 +0700

    add thunk action

---
+++ src/redux/actionType/filteredData.actionType.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const FETCH_FILTERED_DATA = 'filteredData/fetchFilteredData';
---
+++ src/redux/actionType/searchByType.actionType.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const SEARCH_BY_TYPE_FETCH_TITLES = 'searchByType/fetchTitles';
