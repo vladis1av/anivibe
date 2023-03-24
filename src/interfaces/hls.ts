62b0ab648af2cf5269e7a07163f6a04a964a10cf
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 05:00:46 +0700

    update hls types

--- src/interfaces/hls.ts
+++ src/interfaces/hls.ts
@@ -1,6 +1,7 @@
-import { EHlsQuality } from '@enums/enums';
+import { EHlsQuality, EHlsQualityKey } from '@enums/enums';
 
 export type EHlsQualityType = (keyof typeof EHlsQuality);
+export type EHlsQualityKeyType = (keyof typeof EHlsQualityKey);
 
 export type HlsQuality = {
   [key in EHlsQualityType]: string;
