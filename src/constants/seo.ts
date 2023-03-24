0d1cb60a50c453123a51af2b62b7a57f0ff37047
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-03-25 04:55:11 +0700

    add seo constants

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,14 +1,29 @@
-export const APP_NAME: string = 'Anime APP';
-
-export const SEO_TITLE: string = `Читать мангу или смотреть аниме и скачать его через торрент на ${APP_NAME}`;
-
-export const SEO_DESCRIPTION: string = `На сайте вы можете смотреть онлайн аниме скачать его через торрент,
- а также читать мангу`;
-
-export const SEO_KEYWORDS_APP: string = `Anime APP,Anime app,anime app,анимe эпп,аниме апп,аниме приложение,
- смотреть аниme,скачать аниме,аниме бесплатно,Скачать аниме,аниме торрент,аниме на русском,
- читать мангу,читать мангу бесплатно,манга бесплатно читать онлайн`;
-
-export const SEO_ANIME_DETAIL_PAGE_TITLE: string = 'Смотреть аниме или скачать его на Anime APP';
-
-export const SEO_READ_MANGA_DESCRIPTION: string = 'Легко и удобно читать онлайн мангу на Anime APP';
+export const APP_NAME: string = 'aniVibe';
+export const APP_NAME_UPPER_CASE: string = APP_NAME.toUpperCase();
+export const ANIME_FILTERS_PAGE_TITLE: string = 'Аниме';
+export const ANIME_FILTERS_PAGE_DESCRIPTION: string = `
+Список аниме на русском. По популярности и жанрам на сайте ${APP_NAME_UPPER_CASE}`;
+export const ANIME_FILTERS_PAGE_KEYWORDS: string = `
+Аниме, Анимэ, Аниме список, Аниме каталог, Анимэ список, Анимэ каталог, Анимэ база,
+аниме, анимэ, аниме список, аниме каталог, анимэ список, анимэ каталог, анимэ база
+`;
+export const MANGA_FILTERS_PAGE_TITLE: string = 'Манга';
+export const MANGA_FILTERS_PAGE_DESCRIPTION: string = `
+Список манги ранобе и.тд на русском. По популярности и жанрам на сайте ${APP_NAME_UPPER_CASE}`;
+export const MANGA_FILTERS_PAGE_KEYWORDS: string = `
+Манга, Манга список, Манга каталог, Манга база,
+манга, манга список, манга каталог, манга база
+`;
+export const SEO_ANIME_WATCH_ONLINE_TEXT: string = 'Смотреть онлайн';
+export const SEO_MANGA_READ_ONLINE_TEXT: string = 'Читать онлайн';
+export const SEO_ANIME_DETAIL_PAGE_TITLE: string = `Смотреть или скачать аниме на ${APP_NAME_UPPER_CASE}`;
+export const SEO_READ_MANGA_DESCRIPTION: string = `
+Легко и удобно читать онлайн мангу ранобе и.тд на ${APP_NAME_UPPER_CASE}`;
+export const SEO_TITLE: string = `${APP_NAME_UPPER_CASE} - Cмотрите аниме или Читайте мангу`;
+export const SEO_DESCRIPTION: string = 'Cмотрите онлайн аниме или скачайте его через торрент,а также читайте мангу';
+export const SEO_KEYWORDS_APP: string = `
+ ANIVIBE, aniVibe, anivibe, AniVibe, ани вайб, аниВайб, АниВаб, Аниме, Анимэ, аниме,
+ смотреть аниme, скачать аниме, анимэ, аниме бесплатно,
+ Скачать аниме, аниме торрент, аниме на русском, скачать аниме через торрент,манга,
+ читать мангу, читать мангу бесплатно, манга бесплатно читать онлайн, манга онлайн, манга на русском
+ `;
