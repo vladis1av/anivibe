af4b3a36516419c4ccef4317eea412f9b104e5e0
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-04-02 20:34:57 +0700

    add getMangaSeoTitle

---
+++ src/utils/getMangaSeoTitle.ts
@@ -0,0 +1,29 @@
+import { EReliaseKeyType } from '@interfaces/common';
+
+import { EMangaReliase, EReliaseKey } from '@enums/enums';
+
+import { APP_NAME_UPPER_CASE } from '@constants/seo';
+
+import replaceLastCharacter from './replaceLastCharacter';
+
+const getTitle = (type: string) => `Легко и удобно читать ${type} онлайн на ${APP_NAME_UPPER_CASE}`;
+
+const getMangaSeoTitle = (mangaType: EReliaseKeyType) => {
+  const reliaseType = EMangaReliase[mangaType];
+  const defaultType = replaceLastCharacter(reliaseType, 'у');
+  const title = getTitle(defaultType);
+
+  switch (mangaType) {
+    case EReliaseKey.manga:
+    case EReliaseKey.manhwa:
+      return title;
+
+    case EReliaseKey.manhua:
+      return getTitle(reliaseType);
+
+    default:
+      return title;
+  }
+};
+
+export default getMangaSeoTitle;
