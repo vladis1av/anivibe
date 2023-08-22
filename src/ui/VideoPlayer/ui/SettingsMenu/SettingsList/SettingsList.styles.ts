93ff151b333f5d5016134e7a536a91bcf5b12c8d
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-08-22 20:20:15 +0700

    add style for switch

--- src/ui/VideoPlayer/ui/SettingsMenu/SettingsList/SettingsList.styles.ts
+++ src/ui/VideoPlayer/ui/SettingsMenu/SettingsList/SettingsList.styles.ts
@@ -1,8 +1,15 @@
 import { makeStyles } from '@mui/styles';
 
+import { EColor } from '@enums/enums';
+
 const useSettingsListStyles = makeStyles(() => ({
   videoPlayerSettingsListItemValueType: {
     padding: '0px 0px !important',
+  },
+  videoPlayerSettingsListItemSwitch: {
+    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+      backgroundColor: EColor.lightBlue,
+    },
   },
 }));
 
