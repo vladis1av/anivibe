ecf12ebaf1e02883a44693854bf472d3c20b8acd
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-08-23 04:07:00 +0700

    little fix logic

--- src/ui/Carousel/CarouselItem/CarouselItem.tsx
+++ src/ui/Carousel/CarouselItem/CarouselItem.tsx
@@ -1,16 +1,29 @@
 import { FC, ReactNode } from 'react';
 
+import clsx from 'clsx';
+
 import useCarouselItemStyles from './CarouselItem.styles';
 
 type CarouselItemProps = {
   children: ReactNode;
   maxWidth?: string;
+  isDisabled?: boolean;
 };
 
-const CarouselItem: FC<CarouselItemProps> = ({ children, maxWidth }) => {
+const CarouselItem: FC<CarouselItemProps> = ({ children, maxWidth, isDisabled }) => {
   const classes = useCarouselItemStyles();
 
-  return <li className={classes.carouselListItem} style={{ maxWidth }}>{children}</li>;
+  return <li
+    className={
+      clsx(
+        classes.carouselListItem,
+        { [classes.disable]: isDisabled },
+      )
+    }
+    style={{ maxWidth }}
+  >
+    {children}
+  </li>;
 };
 
 export default CarouselItem;
