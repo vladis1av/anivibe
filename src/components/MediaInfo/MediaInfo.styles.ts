0ab0e3c782a8af92a7141a7a4fa16b8426c42d00
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-08-26 00:27:10 +0700

    change banner gradient

--- src/components/MediaInfo/MediaInfo.styles.ts
+++ src/components/MediaInfo/MediaInfo.styles.ts
@@ -57,13 +57,46 @@ const useMediaInfoStyles = makeStyles((theme: Theme) => ({
   },
   bannerImageGradient: {
     background: `${theme.palette.mode === ETheme.dark
-      ? 'linear-gradient(180deg, rgba(15,15,15,0) 0, #0f0f0f 100%, #0f0f0f 100%)'
-      : 'linear-gradient(180deg, rgba(255,255,255,0) 0, rgba(255,255,255,1) 100%)'}`,
+      ? `linear-gradient(180deg,transparent,
+        rgba(15,15,15,.009) 6.67%,
+      rgba(15,15,15,.036) 13.33%,
+      rgba(15,15,15,.082) 20%,
+      rgba(15,15,15,.147) 26.67%,
+      rgba(15,15,15,.232) 33.33%,
+      rgba(15,15,15,.332) 40%,
+      rgba(15,15,15,.443) 46.67%,
+      rgba(15,15,15,.557) 53.33%,
+      rgba(15,15,15,.668) 60%,
+      rgba(15,15,15,.768) 66.67%,
+      rgba(15,15,15,.853) 73.33%,
+      rgba(15,15,15,.918) 80%,
+      rgba(15,15,15,.964) 86.67%,
+      rgba(15,15,15,.991) 93.33%,
+      rgba(15,15,15,1))
+      `
+      : `linear-gradient(180deg,transparent,
+        rgba(255,255,255,.009) 6.67%,
+      rgba(255,255,255,.036) 13.33%,
+      rgba(255,255,255,.082) 20%,
+      rgba(255,255,255,.147) 26.67%,
+      rgba(255,255,255,.232) 33.33%,
+      rgba(255,255,255,.332) 40%,
+      rgba(255,255,255,.443) 46.67%,
+      rgba(255,255,255,.557) 53.33%,
+      rgba(255,255,255,.668) 60%,
+      rgba(255,255,255,.768) 66.67%,
+      rgba(255,255,255,.853) 73.33%,
+      rgba(255,255,255,.918) 80%,
+      rgba(255,255,255,.964) 86.67%,
+      rgba(255,255,255,.991) 93.33%,
+      rgba(255,255,255,1))
+      `}`,
     position: 'absolute',
     bottom: 0,
     width: '100%',
     height: '100%',
   },
+
   detailContent: {
     maxWidth: 1200,
     margin: '0 auto',
