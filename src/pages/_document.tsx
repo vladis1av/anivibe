c6fd4fc2a792cb8608090a1d6b3f9c6ccfceaa57
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-08-26 02:33:55 +0700

    add yandex verification test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,7 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <meta name="yandex-verification" content="c348945bfd78b269" />
           <link rel="icon" type="image/png" href="/favicon.ico" />
           <link rel="canonical" href={`${CLIENT_API}`} />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
