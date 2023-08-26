5799f734134e34289d0ac764d2d2698db80b3d24
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-08-26 21:27:12 +0700

    add pageDescription component

---
+++ src/ui/PageDescription/PageDescription.styles.ts
@@ -0,0 +1,20 @@
+import { Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const usePageDescriptionStyles = makeStyles((theme: Theme) => ({
+  pageDescriptionWrapper: {
+    marginBottom: 20,
+  },
+  title: {
+    color: `${theme.palette.text.primary}`,
+    fontSize: 24,
+    marginBottom: 5,
+    fontWeight: 400,
+  },
+  description: {
+    fontSize: '.875rem',
+    color: `${theme.palette.text.secondary}`,
+  },
+}));
+
+export default usePageDescriptionStyles;
---
+++ src/ui/PageDescription/PageDescription.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import { Typography } from '@mui/material';
+import clsx from 'clsx';
+
+import usePageDescriptionStyles from './PageDescription.styles';
+
+type PageDescriptionProps = {
+  title: string;
+  description: string;
+  className?: string;
+};
+
+const PageDescription: FC<PageDescriptionProps> = ({ title, description, className }) => {
+  const classes = usePageDescriptionStyles();
+
+  return (
+    <header className={clsx(classes.pageDescriptionWrapper, className)}>
+      <Typography className={classes.title} variant="h1">
+        {title}
+      </Typography>
+
+      <p className={classes.description}>
+        {description}
+      </p>
+    </header>
+  );
+};
+
+export default PageDescription;
