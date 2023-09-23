6ceb5400cff9ee7bf5a1494d8c8480b7fdf8eae1
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-09-24 03:06:58 +0700

    change logic  for canonical and ogUrl

--- src/components/SeoHead/SeoHead.tsx
+++ src/components/SeoHead/SeoHead.tsx
@@ -1,22 +1,14 @@
 import { FC } from 'react';
 
-import getConfig from 'next/config';
 import Head from 'next/head';
-
-import { IS_SERVER } from '@constants/common';
-
-const { publicRuntimeConfig } = getConfig();
-
-const {
-  CLIENT_API,
-} = publicRuntimeConfig;
 
 type Tags = Array<string> ;
 
 type SeoHeadProps = {
   tabTitle: string;
   title: string;
-  isCanonical?: boolean;
+  canonical?: string;
+  ogUrl?: string
   description: string;
   keywords?: string;
   imageSource?: string;
@@ -33,29 +25,27 @@ const getTags = (tagName: 'video' | 'book', tags?: Tags) => tags && tags.length 
 const SeoHead: FC<SeoHeadProps> = ({
   tabTitle,
   title,
-  isCanonical,
+  canonical,
+  ogUrl,
   description,
   keywords,
   imageSource,
   videoTags,
   bookTags,
-}) => {
-  const fullLing = !IS_SERVER ? document.URL : CLIENT_API as string;
-  return (<Head>
-    <title>{tabTitle}</title>
-    <meta content={title} property="og:title" />
-    <meta content={title} property="twitter:title" />
-    <meta content={description} name="og:description" />
-    <meta content={description} name="twitter:description" />
-    <meta content={description} name="description" />
-    { isCanonical && <link rel="canonical" href={`${fullLing}`} />}
-    <meta content={fullLing} property="og:url" />
-    {getTags('video', videoTags)}
-    {getTags('book', bookTags)}
-    {imageSource && <meta content={imageSource} property="og:image"/>}
-    {imageSource && <meta content={imageSource} property="twitter:image"/>}
-    {keywords && <meta content={keywords} name="Keywords" />}
-  </Head>);
-};
+}) => (<Head>
+  <title>{tabTitle}</title>
+  <meta content={title} property="og:title" />
+  <meta content={title} property="twitter:title" />
+  <meta content={description} name="og:description" />
+  <meta content={description} name="twitter:description" />
+  <meta content={description} name="description" />
+  {canonical && <link rel="canonical" href={canonical} />}
+  {ogUrl && <meta content={ogUrl} property="og:url" />}
+  {getTags('video', videoTags)}
+  {getTags('book', bookTags)}
+  {imageSource && <meta content={imageSource} property="og:image"/>}
+  {imageSource && <meta content={imageSource} property="twitter:image"/>}
+  {keywords && <meta content={keywords} name="Keywords" />}
+</Head>);
 
 export default SeoHead;
