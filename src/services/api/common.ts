21dc1b112f5a0c5b119bc07c1304d026ca7b370d
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-09-28 04:41:27 +0700

    remove @apollo/client and graphql

--- src/services/api/common.ts
+++ src/services/api/common.ts
@@ -1,19 +1,30 @@
+import axios from 'redaxios';
+
 import { BannerImage } from '@interfaces/anime';
 import { ECollectionType } from '@interfaces/collection';
 
-import apolloClient from '@services/apolloClient';
-import GET_MEDIA_INFO from '@services/queries/getMediaInfo';
+import getMediaInfo from '@services/queries/getMediaInfo';
+
+import getEnv from '@utils/getEnv';
+
+const { serverRuntimeConfig: { ANILIST_API } } = getEnv();
 
 // eslint-disable-next-line import/prefer-default-export
 export const getHightQualityBanner = async (id: string, type: ECollectionType): Promise<BannerImage> => {
   try {
-    const { data } = await apolloClient.query({
-      query: GET_MEDIA_INFO(type),
-      variables: { search: `${id}` },
+    const graphqlQuery = JSON.stringify({
+      query: getMediaInfo(type),
+      variables: { search: id },
+    });
+
+    const { data } = await axios.post(ANILIST_API, graphqlQuery, {
+      headers: {
+        'content-type': 'application/json',
+      },
     });
 
     return {
-      bannerImageHightQuality: data.Media.bannerImage,
+      bannerImageHightQuality: data.data.Media.bannerImage,
     };
   } catch (error) {
     console.error(error);
