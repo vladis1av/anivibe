8c7fed7bd811abc3c00a04424c47e666050a6203
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-09-29 17:18:57 +0700

    test env config v1

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -1,38 +1,58 @@
 import getConfig from 'next/config';
 
-const getEnv = () => {
-  const { publicRuntimeConfig } = getConfig();
+type ConfigType = {
+  HOST: string;
+  RUNTIME: string;
+  JPG_URL: string;
+  WEBP_URL: string;
+  REGIONS: string[];
+  ANIME_API: string;
+  ANIME_DOMEN: string;
+  TORRENT_URL: string;
+  ANILIST_API: string;
+  MANGAS_API: string[];
+  ANILIST_API_KEY: string;
+  MANGA_API_NUMBER: string;
+};
 
+const getEnvObj = (config: ConfigType): ConfigType => {
   const {
     HOST,
     RUNTIME,
-    REGIONS,
-    DESU_ME_API,
-    ANILIST_API,
-    DESU_ME_API2,
-    ANILIST_API_KEY,
-    ANILIBRIA_API,
-    ANILIBRIA_DOMEN,
-    DESU_ME_API_NUMBER,
     JPG_URL,
+    REGIONS,
     WEBP_URL,
+    ANIME_API,
+    MANGAS_API,
+    ANILIST_API,
     TORRENT_URL,
-  } = publicRuntimeConfig;
+    ANIME_DOMEN,
+    ANILIST_API_KEY,
+    MANGA_API_NUMBER,
+  } = config;
 
   return {
     HOST,
     RUNTIME,
-    REGIONS,
-    DESU_ME_API,
-    ANILIST_API,
-    DESU_ME_API2,
-    ANILIST_API_KEY,
-    ANILIBRIA_API,
-    ANILIBRIA_DOMEN,
-    DESU_ME_API_NUMBER,
     JPG_URL,
+    REGIONS,
     WEBP_URL,
+    ANIME_API,
+    MANGAS_API,
+    ANILIST_API,
     TORRENT_URL,
+    ANIME_DOMEN,
+    ANILIST_API_KEY,
+    MANGA_API_NUMBER,
+  };
+};
+
+const getEnv = () => {
+  const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
+
+  return {
+    publicRuntimeConfig: getEnvObj(publicRuntimeConfig),
+    serverRuntimeConfig: getEnvObj(serverRuntimeConfig),
   };
 };
 
