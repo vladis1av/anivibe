cefcbece4cef70f40e6ddaceb2950ad0d611d4be
Vlad Frolov <vladfrolov.dev@gmail.com>
2023-10-18 00:04:47 +0700

    add config types

---
+++ src/interfaces/config.ts
@@ -0,0 +1,17 @@
+export type ConfigType = {
+  HOST: string;
+  RUNTIME: string;
+  JPG_URL: string;
+  WEBP_URL: string;
+  REGIONS: string[];
+  ANIME_API: string;
+  ANIME_DOMEN: string;
+  TORRENT_URL: string;
+  ANILIST_API: string;
+  MANGAS_API: string[];
+  ANILIST_API_KEY: string;
+  MANGA_API_NUMBER: string;
+  EDGE_FUNCTIONS_ANIME_API: string;
+  EDGE_FUNCTIONS_MANGA_API: string;
+  EDGE_FUNCTIONS_ANILIST_API: string;
+};
