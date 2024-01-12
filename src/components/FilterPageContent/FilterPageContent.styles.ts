6bb8d4f1785fc3da80b32f168b0b1360f3e1ebe0
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-12 06:52:53 +0300

    add filter page content

---
+++ src/components/FilterPageContent/FilterPageContent.styles.ts
@@ -0,0 +1,33 @@
+import { makeStyles } from '@mui/styles';
+
+const useFilterPageContentStyles = makeStyles(() => ({
+  contentWrapper: {
+    marginTop: 20,
+    height: 'calc(100% - 75px)',
+  },
+  content: {
+    display: 'flex',
+  },
+  pageDescription: {
+    marginBottom: 30,
+  },
+  title: {
+    paddingBottom: 20,
+    fontSize: 24,
+  },
+  filterCardListWrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  paginationWrapperTop: {
+    paddingBottom: 20,
+  },
+  paginationWrapperBottom: {
+    marginTop: 'auto',
+    paddingBottom: 20,
+    paddingTop: 25,
+  },
+}));
+
+export default useFilterPageContentStyles;
