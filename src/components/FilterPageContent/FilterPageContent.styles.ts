c46cb4053cf15659cc9b63c6a1ca23aaef4606b3
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-18 12:16:10 +0300

    add skeleton loader

--- src/components/FilterPageContent/FilterPageContent.styles.ts
+++ src/components/FilterPageContent/FilterPageContent.styles.ts
@@ -23,6 +23,31 @@ const useFilterPageContentStyles = makeStyles(() => ({
   paginationWrapperTop: {
     paddingBottom: 20,
   },
+  loadMoreCardItemList: {
+    width: '100%',
+    display: 'grid',
+    gridGap: '28px 30px',
+    gridTemplateColumns: 'repeat(auto-fill,185px)',
+    justifyContent: 'space-between',
+    flex: '0 1 auto',
+    '@media (min-width: 0) and (max-width: 850px)': {
+      gridTemplateColumns: 'repeat(auto-fill,minmax(150px,1fr))',
+      gridGap: '20px 15px',
+      justifyContent: 'center',
+      display: 'grid',
+    },
+    '@media (min-width: 0) and (max-width: 635px)': {
+      gridTemplateColumns: 'repeat(auto-fill,minmax(120px,1fr))',
+      gridGap: '20px 12px',
+      justifyContent: 'center',
+      display: 'grid',
+    },
+  },
+  cardItemSkeleton: {
+    maxWidth: 185,
+    width: '100%',
+    maxHeight: 271,
+  },
   paginationWrapperBottom: {
     marginTop: 'auto',
     paddingBottom: 20,
