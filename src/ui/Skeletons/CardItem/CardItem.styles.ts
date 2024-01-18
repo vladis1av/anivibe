d155458b9188225d70185579cc9f903133330322
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-18 11:59:58 +0300

    update CardItemSkeleton

--- src/ui/Skeletons/CardItem/CardItem.styles.ts
+++ src/ui/Skeletons/CardItem/CardItem.styles.ts
@@ -1,10 +1,18 @@
 import { makeStyles } from '@mui/styles';
 
 const useCardItemSkeletonStyles = makeStyles(() => ({
+  cardItemSkeletonWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
   cardItemSkeleton: {
     width: 220,
     height: 330,
     borderRadius: 22,
+  },
+  cardItemSkeletonText: {
+    marginTop: 10,
+    margin: '0 auto',
   },
 }));
 
--- src/ui/Skeletons/CardItem/CardItem.tsx
+++ src/ui/Skeletons/CardItem/CardItem.tsx
@@ -9,13 +9,22 @@ import { ESkeleton } from '@enums/enums';
 import useSkeletonTheme from '@hooks/useSkeletonTheme';
 
 import useCardItemSkeletonStyles from './CardItem.styles';
+import TextSkeleton from '../Text/Text';
 
-const CardItemSkeleton: FC<SkeletonProps> = ({ skeletonType, className }) => {
+type CardItemSkeletonProps = SkeletonProps & {
+  withTextSkeleton?: boolean;
+};
+
+const CardItemSkeleton: FC<CardItemSkeletonProps> = ({ skeletonType, withTextSkeleton, className }) => {
   const skeleton = useSkeletonTheme(skeletonType || ESkeleton.pulseAuto);
   const classes = useCardItemSkeletonStyles();
 
   return (
-    <div className={clsx(classes.cardItemSkeleton, skeleton, className)} />
+    <div className={classes.cardItemSkeletonWrapper}>
+      <div className={clsx(classes.cardItemSkeleton, skeleton, className)} />
+
+      {withTextSkeleton && <TextSkeleton width={130} className={classes.cardItemSkeletonText}/>}
+    </div>
   );
 };
 
