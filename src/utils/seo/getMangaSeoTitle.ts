7e6742e807bb04f9945fd44f2726033659be0730
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-19 18:54:20 +0300

    update manga seo title

--- src/utils/seo/getMangaSeoTitle.ts
+++ src/utils/seo/getMangaSeoTitle.ts
@@ -1,29 +1,18 @@
-import { EReliaseKeyType } from '@interfaces/common';
+import { EReleaseKindType } from '@interfaces/common';
 
-import { EMangaReliase, EReliaseKey } from '@enums/enums';
+import { EReleaseKind } from '@enums/enums';
 
+import { MANGA_RELEASE_READ_WORDS } from '@constants/common';
 import { APP_NAME_UPPER_CASE } from '@constants/seo';
-
-import replaceLastCharacter from '../regexp/replaceLastCharacter';
 
 const getTitle = (type: string) => `Легко и удобно читать ${type} онлайн на ${APP_NAME_UPPER_CASE}`;
 
-const getMangaSeoTitle = (mangaType: EReliaseKeyType) => {
-  const reliaseType = EMangaReliase[mangaType];
-  const defaultType = replaceLastCharacter(reliaseType, 'у');
-  const title = getTitle(defaultType);
+const getMangaSeoTitle = (mangaType: EReleaseKindType) => {
+  const mangaRelease = MANGA_RELEASE_READ_WORDS[mangaType]
+    ? MANGA_RELEASE_READ_WORDS[mangaType]
+    : MANGA_RELEASE_READ_WORDS[EReleaseKind.manga];
 
-  switch (mangaType) {
-    case EReliaseKey.manga:
-    case EReliaseKey.manhwa:
-      return title;
-
-    case EReliaseKey.manhua:
-      return getTitle(reliaseType);
-
-    default:
-      return title;
-  }
+  return getTitle(mangaRelease);
 };
 
 export default getMangaSeoTitle;
