a8d5d47f0727660c68ba4a94d24db458493dfea9
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-19 18:48:49 +0300

    add manga titles constant and update imports

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -4,7 +4,9 @@ import {
 } from '@interfaces/common';
 import { SvgIconProps } from '@interfaces/svg';
 
-import { ECollection, EPlaceholder, EVideoPlayerMenu } from '@enums/enums';
+import {
+  ECollection, EMangaOrderBy, EPlaceholder, EReleaseKind, EVideoPlayerMenu,
+} from '@enums/enums';
 
 export const HOUR: number = 3600;
 export const MINUTE: number = 60;
@@ -37,6 +39,7 @@ export const VIDEO_PLAYER_SVG_SIZE: SvgIconProps = { width: 20, height: 20 };
 export const PLAYBACK_SPEED: Array<number> = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
 export const USER_ACTIVITY_EVENTS = ['mousedown', 'mousemove', 'keydown', 'touchstart'];
 export const CIRCLES = [{ begin: 0.2, cx: 0 }, { begin: 0.3, cx: 50 }, { begin: 0.4, cx: 100 }];
+
 export const PLACEHOLDERS: PlaceholdersType = {
   [EPlaceholder.poster]: {
     dark: POSTER_DARK,
@@ -49,24 +52,50 @@ export const PLACEHOLDERS: PlaceholdersType = {
     light: POSTER_ERROR_LIGHT,
   },
 };
+
 export const SETTINGS_MENU_ITEM_TITLE = {
   [EVideoPlayerMenu.quality]: 'Качество',
   [EVideoPlayerMenu.ambientMode]: 'Освещение',
   [EVideoPlayerMenu.playbackRate]: 'Скорость',
   [EVideoPlayerMenu.default]: undefined,
 };
+
 export const SETTINGS_MENU_ITEMS: Array<EVideoPlayerMenuType> = [
   EVideoPlayerMenu.default,
   EVideoPlayerMenu.ambientMode,
   EVideoPlayerMenu.quality,
   EVideoPlayerMenu.playbackRate,
 ];
+
 export const SELECT_SEARCH_TYPES: Array<SelectType> = [
   { name: 'Аниме', type: ECollection.anime },
   { name: 'Манга', type: ECollection.manga },
 ];
+
 export const MAIN_ROUTES_MENU: MainRouteType = {
   home: { title: 'Главная' },
   animes: { title: 'Аниме' },
   mangas: { title: 'Манга' },
+};
+
+export const MANGA_ORDER_BY_SELECT = {
+  [EMangaOrderBy.name]: 'По алфавиту',
+  [EMangaOrderBy.updated]: 'По обновлению',
+  [EMangaOrderBy.popular]: 'По популярности',
+};
+
+export const MANGA_RELEASE_READ_WORDS = {
+  [EReleaseKind.manga]: 'Мангу',
+  [EReleaseKind.manhua]: 'Маньхуа',
+  [EReleaseKind.manhwa]: 'Манхву',
+  [EReleaseKind.one_shot]: 'Ваншот',
+  [EReleaseKind.comics]: 'Комикс',
+};
+
+export const MANGA_RELEASE_READING_WORDS = {
+  [EReleaseKind.manga]: 'Манги',
+  [EReleaseKind.manhua]: 'Маньхуа',
+  [EReleaseKind.manhwa]: 'Манхвы',
+  [EReleaseKind.one_shot]: 'Ваншота',
+  [EReleaseKind.comics]: 'Комикса',
 };
