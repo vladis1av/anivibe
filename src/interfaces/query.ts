a4eaa1af5741e9c09ab2fc64381d652acabeafb7
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-19 18:52:21 +0300

    remove type

--- src/interfaces/query.ts
+++ src/interfaces/query.ts
@@ -1,24 +1,5 @@
 export type QueryType<T> = { query: T };
 
-export type AnimeQuery = {
-  years?: string;
-  genres?: string;
-  seasons?: string;
-  after?: string;
-  voices?: string;
-  page?: string;
-};
-
-export type MangaQuery = {
-  page: string;
-} & Pick<AnimeQuery, 'genres'>;
-
-export type MangaChapterQuery = {
-  page: string;
-  mangaId: string;
-  chapterId: string;
-};
-
 export type VideoPlayerEpisodeQuery = {
   episode: string;
 };
