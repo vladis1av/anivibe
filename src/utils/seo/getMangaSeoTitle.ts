529a5ab2c432bc344f7b50f4d6be534bd8ea8033
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-20 09:44:21 +0300

    change imports

--- src/utils/seo/getMangaSeoTitle.ts
+++ src/utils/seo/getMangaSeoTitle.ts
@@ -1,16 +1,16 @@
-import { EReleaseKindType } from '@interfaces/common';
+import { EMangaReleaseKindType } from '@interfaces/common';
 
-import { EReleaseKind } from '@enums/enums';
+import { EMangaReleaseKind } from '@enums/enums';
 
 import { MANGA_RELEASE_READ_WORDS } from '@constants/common';
 import { APP_NAME_UPPER_CASE } from '@constants/seo';
 
 const getTitle = (type: string) => `Легко и удобно читать ${type} онлайн на ${APP_NAME_UPPER_CASE}`;
 
-const getMangaSeoTitle = (mangaType: EReleaseKindType) => {
+const getMangaSeoTitle = (mangaType: EMangaReleaseKindType) => {
   const mangaRelease = MANGA_RELEASE_READ_WORDS[mangaType]
     ? MANGA_RELEASE_READ_WORDS[mangaType]
-    : MANGA_RELEASE_READ_WORDS[EReleaseKind.manga];
+    : MANGA_RELEASE_READ_WORDS[EMangaReleaseKind.manga];
 
   return getTitle(mangaRelease);
 };
