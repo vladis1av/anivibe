8db490864f30596f7aca32d87b6665cfb2850695
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-23 15:53:03 +0700

    add google verify

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,13 +17,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" href="/favicon.ico" />
           <meta name="yandex-verification" content="c348945bfd78b269" />
           <meta name="google-site-verification" content="lYMieH2JA-N7C-7OngdZvjJlgUfL8EonOJolad0Os74" />
-          <link rel="icon" type="image/png" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <meta name="google-adsense-account" content="ca-pub-9863048209509930" />
           {(this.props as any).emotionStyleTags}
           <Script
             async
