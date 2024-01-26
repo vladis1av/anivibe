4742d21613752ebc5a3d5684f41de98863785e25
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-27 04:30:11 +0700

    fix replace

--- src/utils/regexp/changeDomainZone.ts
+++ src/utils/regexp/changeDomainZone.ts
@@ -2,6 +2,6 @@
 const changeDomainZone = (
   url: string,
   domainZone: string,
-) => url.replace(/(?<=\.).*?(?=\/)/g, domainZone);
+) => url.replace(/(?<=\.).*?(?=\/)/, domainZone);
 
 export default changeDomainZone;
