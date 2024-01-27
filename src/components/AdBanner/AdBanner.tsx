d0be86bff63a08109aeb2d9fb2de6ad059d3d628
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-01-27 22:14:28 +0700

    update logic for ad

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -5,17 +5,19 @@ import {
 import { useRouter } from 'next/router';
 
 type AdBannerProps = {
-  className?: string;
-  style?: CSSProperties;
-  client: string;
   slot: string;
+  client: string;
+  styleAd?: CSSProperties;
+  classNameAd?: string;
+  classNameAdWrapper?: string;
 };
 
 const AdBanner: FC<AdBannerProps> = ({
-  style,
-  className,
-  client,
   slot,
+  client,
+  styleAd,
+  classNameAd,
+  classNameAdWrapper,
 }) => {
   const [isHidden, setIsHidden] = useState(true);
   const insRef = useRef<null | HTMLModElement>(null);
@@ -35,7 +37,7 @@ const AdBanner: FC<AdBannerProps> = ({
     const interval = setInterval(() => {
       // Check if Adsense script is loaded every 300ms
       if (window.MRGtag) {
-        setIsHidden(false);
+        // setIsHidden(false);
         pushAd();
         // clear the interval once the ad is pushed so that function isn't called indefinitely
         clearInterval(interval);
@@ -49,25 +51,32 @@ const AdBanner: FC<AdBannerProps> = ({
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (insRef.current && !insRef.current.hasChildNodes()) {
-        setIsHidden(true);
+      if (insRef.current && insRef.current.hasChildNodes()) {
+        setIsHidden(false);
         clearInterval(interval);
       }
-    }, 2000);
+    }, 400);
 
     return () => {
       clearInterval(interval);
     };
   }, [insRef.current, router.query]);
 
+  const hidenStyles: CSSProperties = isHidden
+    ? { position: 'absolute', top: '-999999px', width: '100%' }
+    : { position: 'static' };
+
   return (
-    <ins
-      ref={insRef}
-      className={className}
-      style={{ ...style, display: isHidden ? 'none' : 'block' }}
-      data-ad-client={client}
-      data-ad-slot={slot}
-    />
+    <div className={classNameAdWrapper} style={hidenStyles}>
+      <ins
+        ref={insRef}
+        className={classNameAd}
+        style={styleAd}
+        data-ad-client={client}
+        data-ad-slot={slot}
+      />
+    </div>
+
   );
 };
 
