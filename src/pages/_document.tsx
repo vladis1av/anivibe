056ad3003903a4f9ba84b76d71989a18bb8017ae
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-01 20:22:35 +0700

    add yandex metrika

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Children } from 'react';
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
+import Script from 'next/script';
 
 import createEmotionServer from '@emotion/server/create-instance';
 import { ServerStyleSheets as JSSServerStyleSheets } from '@mui/styles';
@@ -31,6 +32,28 @@ export default class MyDocument extends Document {
           <Main />
 
           <NextScript />
+
+          <Script id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(96331813, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+               });`
+            }
+          </Script>
+
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/96331813" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
         </body>
       </Html>
     );
