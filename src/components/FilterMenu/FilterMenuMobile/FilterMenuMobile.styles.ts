0d0b4e856e9c5906949ef9f0f8397f281715c255
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-01 08:05:58 +0700

    add dynamic top position

--- src/components/FilterMenu/FilterMenuMobile/FilterMenuMobile.styles.ts
+++ src/components/FilterMenu/FilterMenuMobile/FilterMenuMobile.styles.ts
@@ -3,47 +3,56 @@ import { makeStyles } from '@mui/styles';
 
 import { EColor, ETheme } from '@enums/enums';
 
-const useFilterMenuMobileStyles = makeStyles((theme: Theme) => ({
-  filterMenuTabletAndBelow: {
-    display: 'none',
-    zIndex: 230,
+const useFilterMenuMobileStyles = (headerHeight: number) => {
+  const classes = makeStyles((theme: Theme) => ({
+    filterMenuTabletAndBelow: {
+      display: 'none',
+      zIndex: 230,
 
-    '@media (min-width: 0) and (max-width: 1025px)': {
-      display: 'block',
+      '@media (min-width: 0) and (max-width: 1025px)': {
+        display: 'block',
+      },
     },
-  },
-  filterMenuButton: {
-    color: EColor.white,
-    position: 'fixed',
-    fontWeight: 600,
-    minWidth: 35,
-    height: 35,
-    top: 115,
-    right: '5px',
-    border: `1px solid ${EColor.white}`,
-    padding: 2,
-    backdropFilter: 'blur(10px)',
-    backgroundColor: EColor.halfTransparentBlack,
+    filterMenuButtonTop: {
+      top: headerHeight + 20,
+    },
+    filterMenuButton: {
+      color: EColor.white,
+      position: 'fixed',
+      fontWeight: 600,
+      minWidth: 35,
+      height: 35,
+      right: '5px',
+      border: `1px solid ${EColor.white}`,
+      padding: 2,
+      backdropFilter: 'blur(10px)',
+      backgroundColor: EColor.halfTransparentBlack,
 
-    '&:hover': {
-      opacity: 0.9,
+      '&:hover': {
+        opacity: 0.9,
+      },
     },
-  },
-  filterMenuWrapper: {
-    maxWidth: 310,
-    boxShadow: '0 2px 8px 2px rgb(0 0 0 / 24%)',
-    overflowY: 'auto',
+    filterMenuWrapperTop: {
+      top: headerHeight,
+    },
+    filterMenuWrapper: {
+      maxWidth: 310,
+      boxShadow: '0 2px 8px 2px rgb(0 0 0 / 24%)',
+      overflowY: 'auto',
 
-    '@media (min-width: 0) and (max-width: 1025px)': {
-      top: 60,
-      right: 0,
-      backdropFilter: 'blur(25px)',
-      backgroundColor: theme.palette.mode === ETheme.dark ? EColor.halfTransparentBlack : EColor.halfTransparentWhite,
+      '@media (min-width: 0) and (max-width: 1025px)': {
+        right: 0,
+        backdropFilter: 'blur(25px)',
+        backgroundColor: theme.palette.mode === ETheme.dark ? EColor.halfTransparentBlack : EColor.halfTransparentWhite,
+      },
+
+      '@media (min-width: 0) and (max-width: 450px)': {
+        maxWidth: '100%',
+      },
     },
-    '@media (min-width: 0) and (max-width: 450px)': {
-      maxWidth: '100%',
-    },
-  },
-}));
+  }));
+
+  return classes();
+};
 
 export default useFilterMenuMobileStyles;
