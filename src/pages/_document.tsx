af2fa6be99c1543e44a168b3b0b522a11b96a706
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-08 23:34:05 +0700

    remove metrics for dev mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,8 +33,10 @@ export default class MyDocument extends Document {
 
           <NextScript />
 
-          <Script id="metrika-counter" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          {
+            !IS_DEV && <>
+              <Script id="metrika-counter" strategy="afterInteractive">
+                {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -46,14 +48,20 @@ export default class MyDocument extends Document {
                   accurateTrackBounce:true,
                   webvisor:true
                });`
-            }
-          </Script>
+                }
+              </Script>
 
-          <noscript>
-            <div>
-              <img src="https://mc.yandex.ru/watch/96331813" style={{ position: 'absolute', left: '-9999px' }} alt="" />
-            </div>
-          </noscript>
+              <noscript>
+                <div>
+                  <img
+                    src="https://mc.yandex.ru/watch/96331813"
+                    style={{ position: 'absolute', left: '-9999px' }}
+                    alt=""
+                  />
+                </div>
+              </noscript>
+            </>
+          }
         </body>
       </Html>
     );
