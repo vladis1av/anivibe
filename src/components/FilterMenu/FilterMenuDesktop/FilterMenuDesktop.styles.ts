90df14930878ffcfce22a2717d797e6655a81b91
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-13 20:20:37 +0700

    update return classes

--- src/components/FilterMenu/FilterMenuDesktop/FilterMenuDesktop.styles.ts
+++ src/components/FilterMenu/FilterMenuDesktop/FilterMenuDesktop.styles.ts
@@ -1,22 +1,19 @@
 import { makeStyles } from '@mui/styles';
 
-const useFilterMenuDesktopStyles = (headerHeight: number) => {
-  const classes = makeStyles(() => ({
-    filterMenuDesktop: {
-      marginLeft: 10,
-      position: 'sticky',
-      display: 'block',
-      height: '100%',
-      width: '100%',
-      maxWidth: 316,
-      top: headerHeight,
-      '@media (min-width: 0px) and (max-width: 1025px)': {
-        display: 'none',
-      },
-    },
-  }));
+const useFilterMenuDesktopStyles = (headerHeight: number) => makeStyles(() => ({
+  filterMenuDesktop: {
+    marginLeft: 10,
+    position: 'sticky',
+    display: 'block',
+    height: '100%',
+    width: '100%',
+    maxWidth: 316,
+    top: headerHeight,
 
-  return classes();
-};
+    '@media (min-width: 0px) and (max-width: 1025px)': {
+      display: 'none',
+    },
+  },
+}));
 
 export default useFilterMenuDesktopStyles;
