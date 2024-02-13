43e994c3226e0fc5fcac21fb27f6fbf7db860218
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-13 21:31:33 +0700

    add snackbar container

---
+++ src/ui/SnackbarContainer/SnackbarContainer.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles';
+
+const useSnackbarContainerStyles = makeStyles(() => ({
+  snackbarContainer: {
+    position: 'fixed',
+  },
+}));
+
+export default useSnackbarContainerStyles;
