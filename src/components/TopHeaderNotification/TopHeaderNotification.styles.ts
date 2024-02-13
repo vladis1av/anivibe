a815c09133fc1b6848c46f57907b49183f2d49f2
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-02-13 21:08:36 +0700

    fix style

--- src/components/TopHeaderNotification/TopHeaderNotification.styles.ts
+++ src/components/TopHeaderNotification/TopHeaderNotification.styles.ts
@@ -5,23 +5,23 @@ import { EColor } from '@enums/enums';
 const useTopHeaderNotificationStyles = makeStyles(() => ({
   topHeaderNotification: {
     borderRadius: 0,
-    backgroundColor: EColor.yellow,
     top: 0,
     left: 0,
     width: '100%',
     boxShadow: 'none',
     textAlign: 'center',
     padding: '2px 5px',
-    transition: 'background-color 1000ms linear',
+    position: 'static',
+    alignItems: 'center',
 
-    '& span': {
+    '& div': {
       padding: 0,
-      fontWeight: 500,
       color: EColor.black,
     },
-  },
-  topHeaderNotificationStatic: {
-    position: 'static',
+
+    '& svg': {
+      fill: EColor.black,
+    },
   },
   adblock: {
     backgroundColor: EColor.yellow,
