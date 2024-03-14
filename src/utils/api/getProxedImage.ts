f50f66dca2f3fdf47f6dc328825e8c0df86e728d
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-14 18:08:28 +0700

    add proxy utils

---
+++ src/utils/api/getProxedImage.ts
@@ -0,0 +1,9 @@
+import getNextEnv from '@utils/config/getNextEnv';
+
+const { publicRuntimeConfig: { MANGA_IMAGES_DOMAIN } } = getNextEnv();
+
+const getProxedImage = (
+  img: string,
+) => `/api/proxy/image?referer=https://${MANGA_IMAGES_DOMAIN[0]}/&url=${img}`;
+
+export default getProxedImage;
