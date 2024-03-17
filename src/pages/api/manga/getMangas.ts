95e287b15faf5eecce229c7b8ee2e124f863c545
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-17 18:18:28 +0700

    asd

--- src/pages/api/manga/getMangas.ts
+++ src/pages/api/manga/getMangas.ts
@@ -2,12 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import generateQuery from '@utils/api/generateQuery';
-import getApiByNumber from '@utils/api/getApiByNumber';
-import getNextEnv from '@utils/config/getNextEnv';
-
-const { publicRuntimeConfig: { MANGAS_API, MANGA_API_NUMBER, MANGA_IMAGES_DOMAIN } } = getNextEnv();
-
-const CURRENT_MANGA_API = getApiByNumber(MANGAS_API, Number(MANGA_API_NUMBER), MANGAS_API[0]);
 
 export const config = {
   api: {
@@ -34,7 +28,6 @@ export default async function handler(
     kinds,
     order,
   } = req.query;
-  console.log('CURRENT_MANGA_API', CURRENT_MANGA_API);
   try {
     const query = generateQuery({
       page,
@@ -44,19 +37,15 @@ export default async function handler(
       kinds,
       order,
     });
-    const HOST = MANGA_IMAGES_DOMAIN[0];
-    const data = await fetch(`${CURRENT_MANGA_API}?${query}`, {
+    const data = await fetch(`https://desu.win/manga/api?${query}`, {
       method: 'GET',
       headers: { ...req.headers } as any,
     });
-    console.error('HOST', HOST);
-    console.error('CURRENT_MANGA_API', CURRENT_MANGA_API);
 
     const result = await data.json();
     res.status(200).json(result);
   } catch (error) {
     console.error(error);
-    console.error('catch CURRENT_MANGA_API', CURRENT_MANGA_API);
     res.status(500).json({ data: null });
   }
 }
