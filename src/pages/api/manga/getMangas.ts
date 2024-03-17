1dbc672e8fb218dff4f7a2e1bd7ff47842b749ea
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-17 18:05:45 +0700

    asd

--- src/pages/api/manga/getMangas.ts
+++ src/pages/api/manga/getMangas.ts
@@ -47,22 +47,7 @@ export default async function handler(
     const HOST = MANGA_IMAGES_DOMAIN[0];
     const data = await fetch(`${CURRENT_MANGA_API}?${query}`, {
       method: 'GET',
-      headers: {
-        authority: HOST,
-        referer: `https:/${HOST}/`,
-        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'ru,en;q=0.9',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "YaBrowser";v="24.1", "Yowser";v="2.5"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-      },
+      headers: { ...req.headers } as any,
     });
     console.error('HOST', HOST);
     console.error('CURRENT_MANGA_API', CURRENT_MANGA_API);
