b2677d8dd6c9cf88ba85eadf3dfd4a6e27fcbd60
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-17 07:36:24 +0700

    fix api

--- src/pages/api/manga/getMangaChapter.ts
+++ src/pages/api/manga/getMangaChapter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import axios from 'redaxios';
@@ -36,7 +37,19 @@ export default async function handler(
       encodeURI(`${CURRENT_MANGA_API}${mangaId}/chapter/${chapterId}`),
       {
         headers: {
-          referer: MANGA_IMAGES_DOMAIN[0],
+          authority: MANGA_IMAGES_DOMAIN[0],
+          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'accept-language': 'ru,en;q=0.9',
+          'cache-control': 'max-age=0',
+          'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "YaBrowser";v="24.1", "Yowser";v="2.5"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+          'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
         },
       },
     );
