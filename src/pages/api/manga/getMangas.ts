bc0e5040f8979eb154da81d962baf805dc30bbe0
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-17 07:54:37 +0700

    api fetch

--- src/pages/api/manga/getMangas.ts
+++ src/pages/api/manga/getMangas.ts
@@ -1,8 +1,6 @@
 /* eslint-disable max-len */
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import axios from 'redaxios';
-
 import generateQuery from '@utils/api/generateQuery';
 import getApiByNumber from '@utils/api/getApiByNumber';
 import getNextEnv from '@utils/config/getNextEnv';
@@ -36,7 +34,7 @@ export default async function handler(
     kinds,
     order,
   } = req.query;
-
+  console.log('CURRENT_MANGA_API', CURRENT_MANGA_API);
   try {
     const query = generateQuery({
       page,
@@ -46,31 +44,29 @@ export default async function handler(
       kinds,
       order,
     });
-
-    const { data } = await axios.get(
-      encodeURI(`${CURRENT_MANGA_API}?${query}`),
-      {
-        headers: {
-          authority: MANGA_IMAGES_DOMAIN[0],
-          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-          'accept-language': 'ru,en;q=0.9',
-          'cache-control': 'max-age=0',
-          'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "YaBrowser";v="24.1", "Yowser";v="2.5"',
-          'sec-ch-ua-mobile': '?1',
-          'sec-ch-ua-platform': '"Android"',
-          'sec-fetch-dest': 'document',
-          'sec-fetch-mode': 'navigate',
-          'sec-fetch-site': 'none',
-          'sec-fetch-user': '?1',
-          'upgrade-insecure-requests': '1',
-          'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-        },
+    const data = await fetch(`${CURRENT_MANGA_API}?${query}`, {
+      method: 'GET',
+      headers: {
+        authority: MANGA_IMAGES_DOMAIN[0],
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'ru,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "YaBrowser";v="24.1", "Yowser";v="2.5"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
       },
-    );
-
-    res.status(200).json(data);
+    });
+    const result = await data.json();
+    res.status(200).json(result);
   } catch (error) {
     console.error(error);
+    console.log('CURRENT_MANGA_API', CURRENT_MANGA_API);
     res.status(500).json({ data: null });
   }
 }
