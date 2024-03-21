12599bf8402a63bef8447ce28511fd174e24c09b
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-22 00:28:05 +0700

    change proxy

--- src/utils/api/getProxedImage.ts
+++ src/utils/api/getProxedImage.ts
@@ -1,9 +1,5 @@
-import getNextEnv from '@utils/config/getNextEnv';
-
-const { publicRuntimeConfig: { MANGA_IMAGES_DOMAIN } } = getNextEnv();
-
 const getProxedImage = (
   img: string,
-) => `/api/proxy-image?referer=https://${MANGA_IMAGES_DOMAIN[0]}/&url=${img}`;
+) => `https://trace.moe/image-proxy?url=${img}`;
 
 export default getProxedImage;
