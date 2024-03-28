7de7ad417081b6fb2c0417939cb631e4748ab82d
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-03-29 01:00:34 +0700

    change proxy

--- src/utils/api/getProxedImage.ts
+++ src/utils/api/getProxedImage.ts
@@ -4,6 +4,7 @@ const { publicRuntimeConfig: { MANGA_IMAGES_DOMAIN } } = getNextEnv();
 
 const getProxedImage = (
   img: string,
-) => `/api/proxy-image?referer=https://${MANGA_IMAGES_DOMAIN[0]}/&url=${img}`;
+// eslint-disable-next-line max-len
+) => `https://test-railway-production-322f.up.railway.app/api/v1/proxy?referer=https://${MANGA_IMAGES_DOMAIN[0]}/&url=${img}`;
 
 export default getProxedImage;
