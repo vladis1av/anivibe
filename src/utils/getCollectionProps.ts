c3065a32c50f87d41d48586ad4838dae4ed23742
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-08 07:08:02 +0700

    update props

--- src/utils/getCollectionProps.ts
+++ src/utils/getCollectionProps.ts
@@ -1,7 +1,10 @@
 import { FetchedLastAnimeUpdatedResult } from '@interfaces/anime/service';
+import { EReleaseType } from '@interfaces/common';
 import { MangaBase } from '@interfaces/manga/manga';
 
 import isMangaCollectionTypeGuard from '@typeGuards/isMangaCollection';
+
+import { ERelease } from '@enums/enums';
 
 import { CardItemProps } from '@ui/CardItem/CardItem';
 
@@ -15,27 +18,34 @@ const { publicRuntimeConfig: { MANGA_IMAGE_POSTER_DOMAIN } } = getNextEnv();
 
 const getCollectionProps = (item: MangaBase | FetchedLastAnimeUpdatedResult): CardItemProps => {
   const hideTitle = true;
+  const hideType = true;
 
   if (isMangaCollectionTypeGuard(item)) {
     const {
-      id, image, name, russian,
+      id, image, name, russian, kind,
     } = item;
 
     return {
       id,
       hideTitle,
+      hideType,
       title: russian,
+      type: ERelease[kind as EReleaseType] || '',
       imageSource: changeDomainZone(image.preview, MANGA_IMAGE_POSTER_DOMAIN),
       pathTo: formatMangaPath(id, name),
     };
   }
 
-  const { id, code, names } = item;
+  const {
+    id, code, names, type,
+  } = item;
 
   return {
     id,
     hideTitle,
+    hideType,
     title: names.ru,
+    type: ERelease[type.string.toLowerCase() as EReleaseType] || '',
     pathTo: formatAnimePath(id, code),
   };
 };
