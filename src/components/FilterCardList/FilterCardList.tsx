f4dac9341274fa0a469d93c415d280f58ecb8f4e
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-08 07:18:46 +0700

    add year and type

--- src/components/FilterCardList/FilterCardList.tsx
+++ src/components/FilterCardList/FilterCardList.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { FC } from 'react';
 
+import { EReleaseType } from '@interfaces/common';
+
 import isMangaBaseType from '@typeGuards/isMangaBaseType';
+
+import { ELocale, ERelease } from '@enums/enums';
 
 import { FilteredData } from '@redux/slices/filteredData';
 
 import CardItem from '@ui/CardItem';
 
 import getNextEnv from '@utils/config/getNextEnv';
+import getDateFromUnix from '@utils/date/getDateFromUnix';
+import getFormatedDate from '@utils/date/getFormatedDate';
 import formatAnimePath from '@utils/formatting/formatAnimePath';
 import formatMangaPath from '@utils/formatting/formatMangaPath';
 import changeDomainZone from '@utils/regexp/changeDomainZone';
@@ -28,27 +35,38 @@ const FilterCardList: FC<FilterCardListProps> = ({
     {
       filteredList.map((item) => {
         if (!isMangaBaseType(item)) {
-          const { id, names, code } = item;
+          const {
+            id, names, code, season, type,
+          } = item;
 
           return <CardItem
-            key={id}
             id={id}
+            key={id}
             title={names.ru}
+            year={season.year}
             pathTo={formatAnimePath(id, code)}
+            type={ERelease[type.string.toLowerCase() as EReleaseType] || ''}
           />;
         }
 
         const {
-          id, russian, name, image,
+          id, russian, name, image, aired_on, kind,
         } = item;
 
         return <CardItem
-          key={id}
           id={id}
+          key={id}
+          year={Number(getFormatedDate({
+            date: getDateFromUnix(aired_on),
+            locale: ELocale.ru,
+            withCustomFormat: true,
+            dateOptions: { year: 'numeric' },
+          }))}
           title={russian}
-          imageSource={changeDomainZone(image.preview, MANGA_IMAGE_POSTER_DOMAIN)}
-          pathTo={formatMangaPath(id, name)}
           className={classes.cardListItem}
+          pathTo={formatMangaPath(id, name)}
+          imageSource={changeDomainZone(image.preview, MANGA_IMAGE_POSTER_DOMAIN)}
+          type={ERelease[kind as EReleaseType] || ''}
         />;
       })
     }
