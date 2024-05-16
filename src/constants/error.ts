3b75029e8b7cf22bd95e44cfd82d27aeb8b1578a
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-16 10:18:49 +0700

    update errors

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-export const NOT_FOUND_MANGA_ERROR: string = 'Похоже, такой манги у нас пока нет';
-export const NOT_FOUND_ANIME_ERROR: string = 'Похоже, такого аниме у нас пока нет';
-export const NOT_FOUND_CHAPTER_ERROR: string = 'Похоже, такой главы у нас пока нет';
+export const NOT_FOUND_MANGA_ERROR: string = 'Похоже, такой манги у нас пока нет, обновите страницу или перейдите на другую';
+export const NOT_FOUND_ANIME_ERROR: string = 'Похоже, такого аниме у нас пока нет, обновите страницу или перейдите на другую';
+export const NOT_FOUND_CHAPTER_ERROR: string = 'Похоже, такой главы у нас пока нет, обновите страницу или перейдите на другую';
 export const COMMON_ERROR: string = 'Упс, что-то пошло не так, попробуйте зайти на другую страницу или обновить текущую';
 export const NOT_FOUND_ERROR: string = 'Похоже этой страницы не существует, попробуйте зайти на другую';
