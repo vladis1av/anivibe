52674a20c54fbf3ef31b105e8e7e4cca13366f39
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-23 04:06:26 +0700

    add blocked ids

--- src/constants/block.ts
+++ src/constants/block.ts
@@ -1,4 +1,35 @@
+import { BlockType } from '@interfaces/common';
+
+import { COPYRIGHT_BLOCK_ERROR, RKN_BLOCK_ERROR } from './error';
+
 // eslint-disable-next-line import/prefer-default-export
-export const BLOCK_ID_LIST: string[] = [
-  '2705-roses-and-champagne',
+export const BLOCK_ID_LIST: Array<BlockType> = [
+  {
+    id: '2705-roses-and-champagne',
+    rkn: RKN_BLOCK_ERROR,
+  },
+  {
+    id: '131-noragami',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
+  {
+    id: '395-nanatsu-no-taizai',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
+  {
+    id: '10-fairy-tail',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
+  {
+    id: '3106-gunnm-mars-chronicle',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
+  {
+    id: '118-blame',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
+  {
+    id: '1769-bakemonogatari',
+    rkn: COPYRIGHT_BLOCK_ERROR,
+  },
 ];
